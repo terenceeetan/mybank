2ece70d5479959daddc88df421ba5db6ced8a1ae
terenceeetan <dakrowl@gmail.com>
2023-09-21 23:30:46 +0800

    Completed task

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const mysql = require('mysql2'); // Import the mysql2 library cause we are using mysql database
+require('dotenv').config();
+
+// Create a MySQL connection pool
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
 const app = express();
 const port = 3000;
 
@@ -19,12 +30,25 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
-  if (username === "bob" && password === "1234") {
-    req.session.isLoggedIn = true;
-    res.redirect(req.query.redirect_url ? req.query.redirect_url : "/");
-  } else {
-    res.render("login", { error: "Username or password is incorrect" });
-  }
+  // Query the database for the user with the provided username and password
+  db.query(
+    'SELECT * FROM accounts WHERE username = ? AND password = ?',
+    [username, password],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.render("login", { error: "An error occurred while processing your request" });
+      } else if (results.length > 0) {
+        // If a user with matching credentials is found, set the session as authenticated
+        req.session.isLoggedIn = true
+        req.session.userID = results[0]['id']
+        res.redirect(req.query.redirect_url ? req.query.redirect_url : "/");
+      } else {
+        // If no matching user is found, render the login page with an error message
+        res.render("login", { error: "Username or password is incorrect" });
+      }
+    }
+  );
 });
 
 /** Handle logout function */
@@ -56,6 +80,61 @@ app.get("/account", (req, res) => {
 
 app.get("/contact", (req, res) => {
   res.send("Our address : 321 Main Street, Beverly Hills.");
+});
+
+app.get("/update_email_address", (req, res) => {
+  if (req.session.isLoggedIn === true) {
+    res.render(view= "update_email");
+  } else {
+    res.redirect("/login?redirect_url=/update_email_address");
+  }
+})
+
+app.post("/update_email_address", (req, res) => {
+  const { old_email, new_email, confirm_email } = req.body;
+  if (new_email != confirm_email) {
+    res.render("update_email", {error: "New email address does not match"}) 
+  }
+  db.query(
+    "UPDATE accounts SET email=(?) WHERE id =? AND email=?",
+    [new_email, req.session.userID, old_email],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        res.render("login", { error: "An error occurred while processing your request" });
+      } else if (results.affectedRows > 0) {
+        // When affectedRows more than 0 it means that the change was successful
+        res.render("update_email", {success: "Email updated successfully"});
+      } else {
+        // If no matching user is found, render the update email view with an error message
+        res.render("update_email", { error: "Your email does not match your account's record" });
+      }
+    }
+  );
+})
+
+app.get("/user", (req, res) => {
+  if (req.session.isLoggedIn === true) {
+    db.query(
+      'SELECT * FROM accounts WHERE id = ?',
+      [req.session.userID],
+      (err, results) => {
+        if (err) {
+          console.error(err);
+          res.render("login", { error: "An error occurred while processing your request" });
+        } else if (results.length > 0) {
+          // Managed to find user based on user ID, pass data as context to user view
+          res.render("user", {username: results[0]['username'], password: results[0]['password'], email_address: results[0]['email']});
+        } else {
+          // If no matching user is found, render the login page with an error message
+          res.render("login", { error: "An error occurred while processing your request" });
+        }
+      }
+    );
+    
+  } else {
+    res.redirect("/login?redirect_url=/user");
+  }
 });
 
 /** App listening on port */
